783d78d61348d775d021d1b83cf753b6757391cd
D3ryk <vaclav.telensky@seznam.cz>
2017-02-14 13:36:16 +0100

    Zakladni datove typy PHP + zakladni operatory v PHP

---
+++ Promenne/demoDatoveTypy.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+    Demonstarace základních datových typů PHP
+*/
+$a = 1;
+$b = 2.5;
+$c = "FooBar";
+$d = true; // boolean muze nabyvat hodnot pouze true a false
+$e = array(1, 2, 3);
+$f = array('klic' => 'hodnota', 'klic1' => 'hodnota1');
+
+var_dump($a, $b, $c, $d, $e, $f);
