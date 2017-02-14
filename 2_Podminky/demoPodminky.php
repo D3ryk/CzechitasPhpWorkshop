691bb77e8de19f51ea7b9ddb382e3558a1caa50c
D3ryk <vaclav.telensky@seznam.cz>
2017-02-14 14:57:52 +0100

    Priprava demonstrace podminek

---
+++ 2_Podminky/demoPodminky.php
@@ -0,0 +1,27 @@
+<?php
+
+echo "Zacatek\n"; 
+
+if (true) {
+    echo "If vetev prvni podminky\n";
+}
+
+echo "Za prvni podminkou\n";
+
+if (false) {
+    echo "If vetev druhe podminky\n";
+} else {
+    echo "Else vetev druhe podminky\n";
+}
+
+echo "Za druhou podminkou\n";
+
+if (false) {
+    echo "If vetev treti podminky\n";
+} else if (false) {
+    echo "Else If vetev treti podminky\n";
+} else {
+    echo "Else vetev treti podminky\n";
+}
+
+echo "Konec";
