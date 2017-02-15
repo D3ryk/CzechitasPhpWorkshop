e513b05ac0b864d76fe5ac773f1a0fc4be2589c1
D3ryk <vaclav.telensky@seznam.cz>
2017-02-15 19:33:39 +0100

    Soubory + Formatovani retezcu

---
+++ 5_Soubory_FormatovaniRetezcu/demoFormatovaniRetezcu.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+    sprintf PHPDoc: http://php.net/manual/en/function.sprintf.php
+**/
+
+$float_number = 5.31649236;
+echo sprintf("%.2f\n", $float_number);
---
+++ 5_Soubory_FormatovaniRetezcu/ukol5/ukol5.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+/* 
+    Napověda pro bod c)
+        Využijte funkci explode PHPDoc: http://php.net/manual/en/function.explode.php
+        explode('\n', $file_content) ---> vrátí pole, kde prvkem pole je řádek souboru .csv
+        explode(',', $file_line) ---> vrátí pole s prvky, které jsou v souboru oddělené čárkami
+*/
+
+$file_content = file_get_contents('./osoby.csv');
+
+/*for(...) {
+    
+}*/
