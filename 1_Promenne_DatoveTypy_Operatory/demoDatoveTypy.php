8fdbb1b239164449df35928d13012ee5bbfe2b2c
D3ryk <vaclav.telensky@seznam.cz>
2017-02-16 12:58:53 +0100

    Oprava komentare

--- 1_Promenne_DatoveTypy_Operatory/demoDatoveTypy.php
+++ 1_Promenne_DatoveTypy_Operatory/demoDatoveTypy.php
@@ -8,7 +8,7 @@ $b = 2.5;
 $c = "FooBar";
 $d = true; // boolean muze nabyvat hodnot pouze true a false
 $e = array(1, 2, 3); // stary zapis
-$f = [1, 2, 3];      // novy zapis - tedy ten spravny
+$f = [1, 2, 3];      // novy zapis
 $g = ['key' => 'value', 'key1' => 'value'];
 
 var_dump($a, $b, $c, $d, $e, $f);
