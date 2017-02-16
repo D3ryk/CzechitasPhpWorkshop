379063ad06869e49ac512fd3c34ad89cb9d21391
travnj01 <jan.travnicek@dixonscarphone.com>
2017-02-16 09:40:55 +0100

    use new array

--- 1_Promenne_DatoveTypy_Operatory/demoDatoveTypy.php
+++ 1_Promenne_DatoveTypy_Operatory/demoDatoveTypy.php
@@ -7,7 +7,8 @@ $a = 1;
 $b = 2.5;
 $c = "FooBar";
 $d = true; // boolean muze nabyvat hodnot pouze true a false
-$e = array(1, 2, 3);
-$f = array('key' => 'value', 'key1' => 'value');
+$e = array(1, 2, 3); // stary zapis
+$f = [1, 2, 3];      // novy zapis - tedy ten spravny
+$g = ['key' => 'value', 'key1' => 'value'];
 
 var_dump($a, $b, $c, $d, $e, $f);
--- 5_Soubory_FormatovaniRetezcu/ukol5/ukol5.php
+++ 5_Soubory_FormatovaniRetezcu/ukol5/ukol5.php
@@ -5,8 +5,8 @@
 /* 
     Napověda pro bod c)
         Využijte funkci explode PHPDoc: http://php.net/manual/en/function.explode.php
-        explode('\n', $file_content) ---> vrátí array, kde prvkem array je řádek souboru .csv
-        explode(',', $file_line) ---> vrátí array s prvky, které jsou v souboru oddělené čárkami
+        explode('\n', $file_content) ---> vrátí pole, kde prvkem pole je řádek souboru .csv
+        explode(',', $file_line) ---> vrátí pole s prvky, které jsou v souboru oddělené čárkami
 */
 
 $file_content = file_get_contents('./osoby.csv');
