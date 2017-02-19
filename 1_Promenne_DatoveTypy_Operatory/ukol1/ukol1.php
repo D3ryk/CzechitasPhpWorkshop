5fb51f7188f3d9c32d0e896d9b5b25b6c5ebe312
D3ryk <vaclav.telensky@seznam.cz>
2017-02-19 10:41:53 +0100

    Reseni prvniho ukolu

--- 1_Promenne_DatoveTypy_Operatory/ukol1/ukol1.php
+++ 1_Promenne_DatoveTypy_Operatory/ukol1/ukol1.php
@@ -2,3 +2,14 @@
 
 $variable1 = 4;
 $variable2 = 2.5;
+
+$variable3 = $variable1 + $variable2;
+$variable1--;
+$variable2++;
+
+$mojePromenna = "MujPrvniRetezec";
+
+$mojePromenna .= " Rozsireni";
+
+var_dump($variable1, $variable2, $variable3, $mojePromenna);
+
