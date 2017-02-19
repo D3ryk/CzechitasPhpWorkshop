5516cc0a5bb8a2c04d358a4244d7a4c9dc3348b9
D3ryk <vaclav.telensky@seznam.cz>
2017-02-19 12:21:12 +0100

    Reseni ukolu3

--- 3_Cykly/ukol3/ukol3.php
+++ 3_Cykly/ukol3/ukol3.php
@@ -12,3 +12,46 @@ $fruits = [
     'Hrozny',
     'Mango'
 ];
+
+echo "Ukol a)\n";
+
+for ($num = 1; $num <= 9; $num++) {
+    echo $num . "\n";
+}
+
+
+echo "Ukol b) \n";
+$total_count = 0;
+foreach ($numbers as $number) {
+    $total_count += $number;
+}
+
+echo $total_count . "\n";
+
+
+echo "Ukol c)";
+foreach ($numbers as $number) {
+    if (($number % 2) == 0) {
+        echo $number . "\n";
+    }
+}
+
+echo "Ukol d)\n";
+$fruit_count = 0;
+foreach ($fruits as $fruit) {
+    $fruit_count = $fruit_count + 1;
+}
+echo $fruit_count . "\n";
+
+
+echo "Ukol e)\n";
+foreach ($fruits as $fruit_key => $fruit_value) {
+    if ($fruit_key == '') {
+        echo $fruit_value . "\n";
+    }
+}
+
+echo "Ukol f)\n";
+for ($i = count($fruits) - 1; $i > 0; $i--) {
+    echo $fruits[$i] . "\n";
+}
