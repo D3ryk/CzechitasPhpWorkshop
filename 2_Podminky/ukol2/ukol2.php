567f1fb06ba9e5cfdec1985544fa832570a43b7c
D3ryk <vaclav.telensky@seznam.cz>
2017-02-19 11:39:41 +0100

    Reseni ukolu2

--- 2_Podminky/ukol2/ukol2.php
+++ 2_Podminky/ukol2/ukol2.php
@@ -2,4 +2,22 @@
 
 $number1 = 6;
 $number2 = 4;
-$day = "";
+$day = "nedele";
+
+if ($number1 > $number2) {
+    echo $number1 . "\n";
+} else {
+    echo $number2 . "\n";
+}
+
+if (($number1 + $number2) > 10) {
+    echo "Soucet je vetsi nez deset\n";
+}
+
+if ($day == "nedele") {
+    echo "Dnes je nedele\n";
+}
+
+if ($number1 <= 4 && $number1 >= 8) {
+    echo 'Cislo je v intervalu od 4 do 8';
+}
