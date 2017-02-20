3814120b94b7d7442b06e085fcf889079b5795af
D3ryk <vaclav.telensky@seznam.cz>
2017-02-20 18:47:44 +0100

    Reseni ukolu 4 a 5

--- 4_Funkce/ukol4/ukol4.php
+++ 4_Funkce/ukol4/ukol4.php
@@ -6,8 +6,30 @@
 
 ************************/
 
+function vypisDatumCas($datum, $cas) {
+    echo "Datum: $datum, Cas: $cas";
+}
+
+function sectiCisla($cislo1, $cislo2) {
+    return $cislo1 + $cislo2;
+}
+
+function vetsiNez($cislo1, $cislo2) {
+    if ($cislo1 > $cislo2) {
+        return true;
+    }
+
+    return false;
+}
 
 
+function vratPrvniPrvekPole($array) {
+    return $array[0];
+}
+
+function druhaMocnina($cislo) {
+    return $cislo * $cislo;
+}
 
 
 /***********************
@@ -15,3 +37,21 @@
     VOLÁNÍ FUNKCÍ
 
 ***********************/
+
+echo "Ukol a)\n";
+vypisDatumCas('20.02.2017', '15:00');
+
+echo "\nUkol b)\n";
+echo sectiCisla(1, 1);
+
+echo "\nUkol c)\n";
+$jeVetsiNez = vetsiNez(4, 3);
+var_dump($jeVetsiNez);
+
+echo "\nUkol d)\n";
+echo vratPrvniPrvekPole([5, 10, 15, 20]);
+
+echo "\nUkol e)\n";
+echo druhaMocnina(4);
+
+echo "\n";
--- 5_Soubory_FormatovaniRetezcu/ukol5/ukol5.php
+++ 5_Soubory_FormatovaniRetezcu/ukol5/ukol5.php
@@ -1,16 +1,40 @@
 <?php
 
 
+//Ukol a)
+file_put_contents('./data/datum.txt', date('d-m-Y'));
+
+//Ukol b)
+$number = file_get_contents('./data/cislo.txt');
+file_put_contents('./data/cislo.txt', ($number - 10) * 2);
+
 
 /* 
     Napověda pro bod c)
         Využijte funkci explode PHPDoc: http://php.net/manual/en/function.explode.php
-        explode('\n', $file_content) ---> vrátí pole, kde prvkem pole je řádek souboru .csv
-        explode(',', $file_line) ---> vrátí pole s prvky, které jsou v souboru oddělené čárkami
+        explode(PHP_EOL, $file_content) ---> vrátí pole, kde prvkem pole je řádek souboru .csv
+        explode(',', $file_lines[0]) ---> vrátí pole s prvky, které jsou v souboru oddělené čárkami
 */
 
-$file_content = file_get_contents('./data/osoby.csv');
 
-/*for(...) {
-    
-}*/
+// Ukol c)
+$file_content = file_get_contents('./data/osoby.csv');
+$file_lines = explode(PHP_EOL, $file_content);
+
+$persons_age = 0;
+for ($file_line = 1; $file_line < count($file_lines) - 1; $file_line++) {
+    $person_data = explode(',', $file_lines[$file_line]);
+    $persons_age += $person_data[2]; 
+}
+
+echo "Prumerny vek osoby je: " . ($persons_age / (count($file_lines) - 1));
+
+
+// Ukol d)
+$number = 19022017;
+echo sprintf("\n%b, %o, %X\n", $number, $number, $number);
+
+// Ukol e)
+$operation = 'Deleni';
+$result = 10 / 3;
+echo sprintf("%s, %.2f\n", $operation, $result);
